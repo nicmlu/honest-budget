bf1f8772b823c0e291ca4d47c966c78dd2fbd51e
nicmlu <NicoleMLucien@gmail.com>
2020-04-26 11:32:55 -0400

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
     has_many :transactions
     has_many :categories, through: :transactions
     has_secure_password 
+    validates :email, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_11_045846) do
+ActiveRecord::Schema.define(version: 2020_04_11_050803) do
+
+  create_table "budgets", force: :cascade do |t|
+    t.string "name"
+    t.float "income"
+    t.float "expenses"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.text "notes"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_budgets_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.string "store_name"
+    t.float "amount"
+    t.datetime "date"
+    t.integer "user_id", null: false
+    t.integer "budget_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["budget_id"], name: "index_transactions_on_budget_id"
+    t.index ["category_id"], name: "index_transactions_on_category_id"
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,4 +53,8 @@ ActiveRecord::Schema.define(version: 2020_04_11_045846) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "budgets", "users"
+  add_foreign_key "transactions", "budgets"
+  add_foreign_key "transactions", "categories"
+  add_foreign_key "transactions", "users"
 end
